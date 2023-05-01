8f65d590e8761d97749a46a749b2a5354fc13e8c
Keshav Rambhujoo <23krambo@gmail.com>
2023-05-01 23:13:27 +0100

    Scraped - Title - Description - Price - Rating - ASIN - Page Number - Search Result Position - Is Sponsored / Organic - Search Keyword - Date and Time of Scraping

--- web_scraper/scraper/scraper/spiders/amazonspider.py
+++ web_scraper/scraper/scraper/spiders/amazonspider.py
@@ -1,7 +1,6 @@
 import scrapy
 from urllib.parse import quote, urljoin
 from datetime import datetime
-import os
 
 
 class AmazonSpider(scrapy.Spider):
@@ -15,13 +14,12 @@ class AmazonSpider(scrapy.Spider):
         keyword_list = ["ipad"]
         for keyword in keyword_list:
             amazon_url = urljoin(self.base_url, f"s?k={quote(keyword)}&page=1")
-            yield scrapy.Request(url=amazon_url, callback=self.parse_search,
+            yield scrapy.Request(url=amazon_url, callback=self.parse_search, dont_filter=True,
                                  meta={"keyword": keyword, "page": 1})
 
     def parse_search(self, response):
         keyword = response.meta["keyword"]
         page = response.meta["page"]
-        output = list()
 
         # region Sponsor Type 1
         # Get Sponsor Type 1 Elements
@@ -31,91 +29,117 @@ class AmazonSpider(scrapy.Spider):
         sponsor_type_1 = list(filter(None, sponsor_type_1))
 
         for element in sponsor_type_1:
-            output.append({
-                "title": None,
-                "description": None,
-                "price": None,
-                "rating": None,
-                "asin": element,
-                "page_number": page,
-                "search_result_position": 0,
-                "is_sponsored": True,
-                "keyword": keyword,
-                "date_time_scraped": datetime.utcnow().isoformat(),
-            })
-
+            product_url = urljoin(f"{self.base_url}/dp/", element)
+            yield scrapy.Request(url=product_url, callback=self.parse_product_details, dont_filter=True,
+                                 meta={
+                                     "asin": element,
+                                     "page_number": page,
+                                     "search_result_position": 0,
+                                     "is_sponsored": True,
+                                     "keyword": keyword,
+                                     "date_time_scraped": datetime.utcnow().isoformat()
+                                 })
         # endregion
 
-        # region Sponsor Type 2
-        number_sponsored_type_2 = 0
-        prev_search_result_position = 0
+        # region Sponsor Type 2 and Organic
         # Get All ASINs in Order - Element Must Have A "data-index" Value And A "data-asin" Value
+        # Check for 'data-index'
         for node in response.css('[data-index]'):
+
+            title = ""
+            price = ""
+            rating = ""
+            is_sponsored = True
+            date_time_scraped = datetime.utcnow().isoformat()
+
+            # Check for 'data-asin'
             if node.css('[data-asin]'):
-                if node.css('.a-row.a-spacing-micro'):
-                    if node.css('.puis-sponsored-label-text'):
-                        asin = node.css('.a-popover-preload ::attr(id)').get()
-                        asin = asin.replace("a-popover-sp-info-popover-", "")
-                        index = node.css("::attr(data-index)").get()
-                        output.append({
-                            "title": None,
-                            "description": None,
-                            "price": None,
-                            "rating": None,
-                            "asin": asin,
-                            "page_number": page,
-                            "search_result_position": index,
-                            "is_sponsored": True,
-                            "keyword": keyword,
-                            "date_time_scraped": datetime.utcnow().isoformat(),
-                        })
-                        number_sponsored_type_2 += 1
-                        prev_search_result_position = int(index)
-        # endregion
+                asin = node.css('[data-asin] ::attr(data-asin)').get().strip()
+                index = node.css('[data-index] ::attr(data-index)').get().strip()
 
-        # region Organic
-        data_asins = response.css('[data-index]::attr(data-asin)').getall()
+                if asin is not "" and index is not "":
 
-        # Filter Empty Values
-        data_asins = list(filter(None, data_asins))
+                    # Grab Result
+                    if node.css('.a-section.a-spacing-small.a-spacing-top-small'):
 
-        # Remove Sponsor Type 2 from Organic
-        data_asins = data_asins[number_sponsored_type_2:]
-        for index, asin in enumerate(data_asins):
-            output.append({
-                "title": None,
-                "description": None,
-                "price": None,
-                "rating": None,
-                "asin": asin,
-                "page_number": page,
-                "search_result_position": prev_search_result_position + 1 + int(index),
-                "is_sponsored": False,
-                "keyword": keyword,
-                "date_time_scraped": datetime.utcnow().isoformat(),
-            })
+                        # Get Title
+                        title = node.css('.a-size-mini.a-spacing-none.a-color-base.s-line-clamp-2 ::text').get()
+
+                        # Get Price
+                        price = node.css('.a-price ::text').get()
+
+                        # Get Ratings
+                        if node.css('.a-row.a-size-small'):
+                            rating = node.css('.a-icon-alt ::text').get()
+
+                        # Check if Sponsored
+                        if node.css('.a-row.a-spacing-micro'):
+                            if node.css('.puis-sponsored-label-text'):
+                                is_sponsored = True
+                        else:
+                            is_sponsored = False
+
+                        # Get Description
+                        product_url = urljoin(f"{self.base_url}/dp/", asin)
+                        yield scrapy.Request(url=product_url, callback=self.parse_product_details, dont_filter=True,
+                                             meta={
+                                                 "title": title,
+                                                 "price": price,
+                                                 "rating": rating,
+                                                 "asin": asin,
+                                                 "page_number": page,
+                                                 "search_result_position": index,
+                                                 "is_sponsored": is_sponsored,
+                                                 "keyword": keyword,
+                                                 "date_time_scraped": date_time_scraped
+                                             })
 
         # endregion
 
         if page == 1:
             number_of_pages = int(response.css("span.s-pagination-item.s-pagination-disabled ::text").getall()[1].strip())
 
-            # if number_of_pages >= 2:
-            #     for page_number in range(2, (number_of_pages + 1)):
-            #         amazon_search_url = urljoin(self.base_url, f"s?k={quote(keyword)}&page={page_number}")
-            #         yield scrapy.Request(url=amazon_search_url, callback=self.parse_search,
-            #                              meta={"keyword": keyword, "page": page_number})
+            if number_of_pages >= 2:
+                for page_number in range(2, (number_of_pages + 1)):
+                    amazon_search_url = urljoin(self.base_url, f"s?k={quote(keyword)}&page={page_number}")
+                    yield scrapy.Request(url=amazon_search_url, callback=self.parse_search,
+                                         meta={"keyword": keyword, "page": page_number})
 
-        for element in output:
-            yield {
-                "title": element["title"],
-                "description": element["description"],
-                "price": element["price"],
-                "rating": element["rating"],
-                "asin": element["asin"],
-                "page_number": element["page_number"],
-                "search_result_position": element["search_result_position"],
-                "is_sponsored": element["is_sponsored"],
-                "keyword": element["keyword"],
-                "date_time_scraped": element["date_time_scraped"],
-            }
+    def parse_product_details(self, response):
+        description = [bullet.strip() for bullet in response.css("#feature-bullets li ::text").get()]
+
+        # region Handle Sponsored Type 1 and Issues Caused By Amazon Limiting Scraping (E503)
+        # Title
+        try:
+            title = response.meta["title"]
+        except:
+            title = response.css("#productTitle::text").get()
+
+        # Price
+        try:
+            price = response.meta["price"]
+        except:
+            price = response.css('.a-price span[aria-hidden="true"] ::text').get()
+            if not price:
+                price = response.css(".a-price .a-offscreen ::text").get()
+
+        # Rating
+        try:
+            rating = response.meta["rating"]
+        except:
+            rating = response.css("i[data-hook=average-star-rating] ::text").get()
+
+        # endregion
+
+        yield {
+            "title": title,
+            "description": description,
+            "price": price,
+            "rating": rating,
+            "asin": response.meta["asin"],
+            "page_number": response.meta["page_number"],
+            "search_result_position": response.meta["search_result_position"],
+            "is_sponsored": response.meta["is_sponsored"],
+            "keyword": response.meta["keyword"],
+            "date_time_scraped": datetime.utcnow().isoformat(),
+        }
--- web_scraper/web_scraper/settings.py
+++ web_scraper/web_scraper/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# TODO : SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-p3r@f63ne*i2j8stkkb8=ur+07aly*6j=s#ox_v$1_rnupv)@-'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# TODO : SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
