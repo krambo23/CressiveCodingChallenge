608ef657792158812d2edf7b0bed7420c63f29e2
Keshav Rambhujoo <23krambo@gmail.com>
2023-05-02 20:48:47 +0100

    API Can Be Queried Based Upon 'keywords' and 'date_time_scraped'

--- web_scraper/scraper_config/urls.py
+++ web_scraper/scraper_config/urls.py
@@ -6,4 +6,6 @@ app_name = "scraper_config"
 urlpatterns = [
     path("", views.index, name="index"),
     path("api/", views.get_scraped_data, name="api"),
+    path("start/", views.start_scraping, name="start_scraping"),
+    path("logs/", views.get_scraper_logs, name="scraper_logs")
 ]
--- web_scraper/scraper_config/views.py
+++ web_scraper/scraper_config/views.py
@@ -6,11 +6,39 @@ from .serializers import ScrapedTableSerializers
 
 # API Endpoints
 def get_scraped_data(request):
-    data = ScrapedTable.objects.all()
-    serializer = ScrapedTableSerializers(data, many=True)
-    return JsonResponse({"data": serializer.data}, safe=False)
+
+    try:
+        keywords = request.GET["keywords"]
+        date_time_scraped = request.GET["date_time_scraped"]
+        data = ScrapedTable.objects.filter(keyword=keywords, date_time_scraped=date_time_scraped)
+        serializer = ScrapedTableSerializers(data, many=True)
+        return JsonResponse({"data": serializer.data}, safe=False)
+    except:
+        try:
+            keywords = request.GET["keywords"]
+            data = ScrapedTable.objects.filter(keyword=keywords)
+            serializer = ScrapedTableSerializers(data, many=True)
+            return JsonResponse({"data": serializer.data}, safe=False)
+        except:
+            try:
+                date_time_scraped = request.GET["date_time_scraped"]
+                data = ScrapedTable.objects.filter(date_time_scraped=date_time_scraped)
+                serializer = ScrapedTableSerializers(data, many=True)
+                return JsonResponse({"data": serializer.data}, safe=False)
+            except:
+                data = ScrapedTable.objects.all()
+                serializer = ScrapedTableSerializers(data, many=True)
+                return JsonResponse({"data": serializer.data}, safe=False)
 
 
 # Views
 def index(request):
     return HttpResponse("Hello World")
+
+
+def start_scraping(request):
+    return HttpResponse("Scraping Started! - Check Progress On get_scraper_logs()")
+
+
+def get_scraper_logs(request):
+    return HttpResponse("Scraper Logs")
